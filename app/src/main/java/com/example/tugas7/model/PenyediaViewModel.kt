189badce1b66ed03feab19c743537eb3f8d55733
beatrixdaa <beatrixdevanti21@gmail.com>
2023-12-03 23:46:18 +0700

    add PenyediaViewModel

--- app/src/main/java/com/example/tugas7/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugas7/model/PenyediaViewModel.kt
@@ -1,10 +1,23 @@
 package com.example.tugas7.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.tugas7.ui.theme.AplikasiSiswa
 
 
 object PenyediaViewModel {
-    val Factory = viewModelFactory { }
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntityViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa=
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 
