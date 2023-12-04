230cdee3751473f74b46df3b997129f6e34c235f
beatrixdaa <beatrixdevanti21@gmail.com>
2023-12-04 15:10:03 +0700

    revisi halaman home

--- app/src/main/java/com/example/tugas7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/tugas7/ui/theme/halaman/HalamanHome.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -23,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -80,7 +82,7 @@ fun HomeScreen (
             itemSiswa = uiStateSiswa.listSiswa,
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxWidth(0
+                .fillMaxSize()
         )
     }
 }
